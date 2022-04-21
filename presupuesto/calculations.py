6c96467202b251c6c22d5e6cafdf341f6613cd9e
Guillermo de Santos López <guillermodesantos23@gmail.com>
2022-04-21 13:33:42 +0200

    Coefs y costes para SF y construc: correctos

--- presupuesto/calculations.py
+++ presupuesto/calculations.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-from presupuesto.models import Solar_field
-
-
-def calcula_factor(objetos, cost_list, info):
-    num_colect_list = [i.num_colect for i in objetos]
+def calcula_factor(objetos, cost_list, cost_type, info):
     
+    budget_type = {'SF': "solar_field_cost", 'construc': "construction_cost"}
+    cost_field = budget_type[cost_type]
+
+    num_colect_list = [i.num_colect for i in objetos]
+
     '''Cálculo de la referencia, dando prioridad a los proyectos reales'''
     num_colect_user = info.num_colect
     #vol_storage_user = form.vol_storage
@@ -17,7 +18,7 @@ def calcula_factor(objetos, cost_list, info):
     
     project_ref = objetos.filter(num_colect=num_colect_ref)[0] #se podría añadir la condición de que sea el proyecto verdadero más reciente
     x = [i/num_colect_ref for i in num_colect_list]
-    cost_f = [i/project_ref.cost for i in cost_list]
+    cost_f = [i/getattr(project_ref, cost_field) for i in cost_list]
     y = [i/j for i, j in zip(cost_f, x)]
     coefs = np.polyfit(np.log(x),np.log(y),1)
     #scipy.optimize.curve_fit(lambda t,b: np.exp(b*t),  x,  y)
@@ -27,7 +28,7 @@ def calcula_factor(objetos, cost_list, info):
     b = coefs[0]
 
     '''Cálculo del coste del campo solar'''
-    SF_cost = (project_ref.solar_field_cost/num_colect_ref)*((b*num_colect_user/num_colect_ref)**a)*num_colect_user
+    estimated_cost = (getattr(project_ref, cost_field)/num_colect_ref)*((a*num_colect_user/num_colect_ref)**b)*num_colect_user
 
 
-    return a, b, SF_cost
+    return a, b, estimated_cost
